736b02c6233a06b80c9f8c1cc467f1941718fe0c
sirius13 <39906774+sirius13@users.noreply.github.com>
2018-06-08 06:26:45 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#include <avr/io.h>
+include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #define F_CPU 8000000UL
@@ -9,7 +9,7 @@ volatile char rxbyte;
 char cnt;
 char input_buffer[64]={0};
 
-//áóôåð äàííûõ íà îòïðàâêó				
+//буфер данных на отправку				
 char SPI_TXbuf[64]={0x00,0x01,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0d,0x0D,0x0E,0x0F,
 					0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,
 					0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,
@@ -26,15 +26,17 @@ volatile char flag1=0;
 short div1000=0;
 char div10=0,div100=0;
 
-void usart_init(void);						//èíèöèàöèÿ UART
-void send_string(char* bptr, char size);	//îîáùåíèé òåðìèíàëà
+void usart_init(void);				/инициация UART
+void send_string(char* bptr, char size);	//посылка строки
+void command_processing(char* b1ptr);		//обработчик сообщений терминала
 
-void spi_init(void);						//èíèöèàöèÿ SPI
-void spi_send_buffer(void);					//íåáëîêèðóþùàÿ îòïðàâêà áóôåðà SPI_TXbuf[64]
-char spi_send_byte(char byte);				//îòïðàâêà áàéòà
+void spi_init(void);				//инициация SPI
+void spi_send_buffer(void);			//неблокирующая отправка буфера SPI_TXbuf[64]
+char spi_send_byte(char byte);			//отправка байта
 
-void tim_3x_start(void); 	//ôîðìèðîâàíèå òàêòîâûõ ñèãíàëîâ 1 Ãö, 10 Ãö è 100 Ãö íà âûâîäàõ PCO, PC1 è PC2
-void tim_pwm_start(void);	//ðåæèì ØÈÌ ãåíåðàòîðà
-void timer_stop(void);		//îñòàíàâëèâàåì ãåíåðàòîð
+void tim_3x_start(void); 	//формирование тактовых сигналов 1 Гц, 10 Гц и 100 Гц на выводах PCO, PC1 и PC2
+void tim_pwm_start(void);	//режим ШИМ генератора
+void timer_stop(void);		//останавливаем генератор
+
 
 
