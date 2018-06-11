007383f4787b55293986bb0458e8240e84947348
sirius13 <39906774+sirius13@users.noreply.github.com>
2018-06-11 09:09:12 +0300

    Update uart.c

--- uart.c
+++ uart.c
@@ -2,18 +2,18 @@
 
 void usart_init(void)
 {
-	UCSRB= (1<<RXCIE)|(1<<RXEN)|(1<<TXEN); ; //разрешить прием, передачу и прерывание по приему байта
+	UCSRB= (1<<RXCIE)|(1<<RXEN)|(1<<TXEN); ; 									//разрешить прием, передачу и прерывание по приему байта
 	UBRRH=0;
-	UBRRL=51;	//Baud rate = 9600
+	UBRRL=51;																	//Baud rate = 9600
 }
 
-void send_string(uint8_t* bptr) //функция отправки данных через терминал
+void send_string(uint8_t* bptr) 												//функция отправки данных через терминал
 {
 		
 	while(*bptr!='\0')
 		{
 			UDR=*bptr;
-			while(!(UCSRA&(1<<UDRE))); //ожидаем окончание передачи байта
+			while(!(UCSRA&(1<<UDRE))); 										//ожидаем окончание передачи байта
 			bptr++;
 		}
 }
@@ -33,34 +33,34 @@ uint8_t command_processing(uint8_t* inbuf)
 	
 	uint8_t i,pin,status=0;
 		
-	if(memcmp(inbuf,osc1,8)==0) 	
+	if(memcmp(inbuf,osc1,8)==0) 												//включить генератор 1/10/100 Гц
 						{
 							timer_stop();
 							tim_3x_start(); 
 							status=1;
 						}
-							
-	if(memcmp(inbuf,osc2,8)==0) 	
+								
+	if(memcmp(inbuf,osc2,8)==0) 												//включить ШИМ-генератор
 						{
 							timer_stop();
 							tim_pwm_start(); 
 							status=1;
 						}
 	
-	if(memcmp(inbuf,oscstop,7)==0)	
-						{
+	if(memcmp(inbuf,oscstop,7)==0)												//остановить генератор
+						{		
 							timer_stop(); 
 							status=1;
 						}
-						
-	if(memcmp(inbuf,pb,5)==0)
+								
+	if(memcmp(inbuf,pb,5)==0)													//если пришла команда управления выводами порта B
 						{
-							pin=strtoul((char*)(inbuf+5),0,10); //находим номер вывода МК
+							pin=strtoul((char*)(inbuf+5),0,10); 				//находим номер вывода МК
 							i=0;
 							
 							while(inbuf[i+1]!='\0') i++;
 							
-							if (inbuf[i]=='h')	
+							if (inbuf[i]=='h')
  
 											{
 												PORTB|=(1<<pin);
@@ -74,9 +74,9 @@ uint8_t command_processing(uint8_t* inbuf)
 											}
 						}
 	
-	if(memcmp(inbuf,pc,5)==0)
+	if(memcmp(inbuf,pc,5)==0)													//если пришла команда управления выводами порта C
 						{
-							pin=strtoul((char*)(inbuf+5),0,10);	//находим номер вывода МК
+							pin=strtoul((char*)(inbuf+5),0,10);				//находим номер вывода МК
 														
 							i=0;
 							while(inbuf[i+1]!='\0') i++;
@@ -95,20 +95,20 @@ uint8_t command_processing(uint8_t* inbuf)
 							status=1;
 						}
 						
-	if(memcmp(inbuf,spibyte,7)==0) 
+	if(memcmp(inbuf,spibyte,7)==0) 											//команда посылки байта по SPI
 									{										
-										SPDR=strtoul((char*)(inbuf+7),0,16); //преобразуем ASCII в hex
-										while(!(SPSR & (1<<SPIF))); //ожидаем окончание передачи байта
-										send_string(answer);
+										SPDR=strtoul((char*)(inbuf+7),0,16); 	//для получения байта преобразуем ASCII в hex
+										while(!(SPSR & (1<<SPIF))); 			//ожидаем окончание передачи байта
+										send_string(answer);					//возвращаем ответ от slave
 										UDR=SPDR;
-										while(!(UCSRA&(1<<UDRE))); //ожидаем окончание передачи байта
+										while(!(UCSRA&(1<<UDRE))); 			
 										send_string(rn);
 										status=1;
 									}
 	
-	if(memcmp(inbuf,spibuff,7)==0) 
+	if(memcmp(inbuf,spibuff,7)==0) 											//команда посылки буфера по SPI
 									{
-										spi_send_buffer(spitx,32); 
+										spi_send_buffer(spitx,31); 
 										status=1;
 									}
 	
